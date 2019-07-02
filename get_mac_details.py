77af77cd0e57834ffa9827ee3be41729f21d48db
araskumara <araskumara@hcl.com>
2019-07-02 23:30:36 +0530

    maximum of 100 characters per line

--- get_mac_details.py
+++ get_mac_details.py
@@ -4,7 +4,8 @@ import sys
 
 
 url_with_mac = \
-    "https://api.macaddress.io/v1?apiKey=at_otwvZGo0omriCX8mhgLEblvsEBRKr&output=json&search={}".format(sys.argv[1])
+    "https://api.macaddress.io/v1?apiKey=at_otwvZGo0omriCX8mhgLEblvsEBRKr&output=json&search={}".\
+        format(sys.argv[1])
 response = requests.get(url_with_mac)
 json_res = response.json()
 print('Company Name: ',json_res['vendorDetails']['companyName'])
